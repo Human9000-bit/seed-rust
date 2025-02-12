68ba40a0efb5311795cae8652b529a4e7f8bb49b
human9000 <human9000.2021@gmail.com>
2025-02-12 17:57:08 +0500

    improved documentation

--- src/seed/entity/message.rs
+++ src/seed/entity/message.rs
@@ -1,9 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+/// Incoming message struct
 #[derive(Serialize, Deserialize)]
 pub struct IncomeMessage {
+    /// Response type
     #[serde(rename = "type")]
     pub rtype: String,
+    /// The message
     pub message: Message,
 }
 
@@ -18,6 +21,7 @@ pub struct Message {
     pub content_iv: String,
 }
 
+/// Outcoming message struct
 #[derive(Serialize, Deserialize)]
 pub struct OutcomeMessage {
     pub nonce: usize,
--- src/traits/websocket.rs
+++ src/traits/websocket.rs
@@ -2,9 +2,14 @@ use actix_ws::{MessageStream, Session};
 
 use crate::seed::entity::{message::IncomeMessage, websocket::WebSocketManager};
 
+/// Repository trait for handling WebSocket operations
 pub trait WebsocketRepository {
+    /// Handles subscription to a chat room
     async fn handle_subscribe(ws: &WebSocketManager, connection: &MessageStream, chat_id: &str);
+    /// Handles unsubscription from a chat room 
     async fn handle_unsubscribe(ws: &WebSocketManager, connection: &MessageStream);
+    /// Broadcasts an event to connected clients
     async fn broadcast_event(ws: &WebSocketManager, message: IncomeMessage);
+    /// Handles client disconnection
     async fn disconnect(ws: &WebSocketManager, connection: &MessageStream);
 }
