7ca8990b001982ca7c2e246bbf8e5f4b688736cc
human9000 <human9000.2021@gmail.com>
2025-02-26 16:22:07 +0500

    implemented websocket connection handling

--- src/infrastructure/websocket.rs
+++ src/infrastructure/websocket.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use actix::{Actor, Context, Handler, Message as ActixMessage, ResponseFuture};
 use actix_ws::Message;
 
 use futures::lock::Mutex;
@@ -18,122 +19,208 @@ use crate::{
     use_case::{messages::MessagesUseCase, websocket::WebSocketUseCase},
 };
 
-pub async fn handle_websocket_connection<DB: MessagesDB, MR: MessagesRepository>(
-    connection: WebSocketConnection,
-    stream: &mut actix_ws::MessageStream,
-    messages_use_case: &MessagesUseCase<DB>,
-    websocket_use_case: &WebSocketUseCase<MR>,
-    ws: &WebSocketManager,
-) {
-    let ws = Arc::new(Mutex::new(ws.clone()));
-    let connection = Arc::new(connection);
+#[derive(Clone)]
+pub struct WebSocketActor<MR: MessagesRepository + Clone, DB: MessagesDB + Clone> {
+    manager: WebSocketManager,
+    websocket_use_case: WebSocketUseCase<MR>,
+    messages_use_case: MessagesUseCase<DB>,
+}
 
-    while let Some(Ok(msg)) = stream.recv().await {
-        if let Message::Text(text) = msg {
-            let incoming: IncomeMessage = serde_json::from_str(&text).unwrap(); // TODO: proper error handling
-            match incoming.rtype.as_str() {
-                "ping" => {
-                    let _ = messages_use_case.status_response(&connection, true).await;
-                }
-                "send" => {
-                    let inner_message = match incoming.message.clone() {
-                        Some(val) => val,
-                        None => {
-                            log::error!("Error parsing send message");
-                            continue;
-                        }
-                    };
-
-                    if !messages_use_case
-                        .is_valid_message(incoming.clone().into())
-                        .await
-                    {
-                        let _ = messages_use_case.status_response(&connection, false).await;
-                        continue;
-                    }
-
-                    let message = entity::websocket::ConnectedMessage {
-                        connection: connection.clone(),
-                        message: incoming,
-                    };
-
-                    let mut ws_lock = ws.lock().await;
-
-                    if ws_lock.message_queues.contains_key(&inner_message.chat_id) {
-                        let _ = ws_lock
-                            .message_queues
-                            .get_mut(&inner_message.chat_id)
-                            .unwrap()
-                            .0
-                            .send(message);
-                        log::info!("Message has been successfully added to the queue");
-                    } else {
-                        log::info!("There is no subscribers to receive message in the queue");
-                        if let Err(err) = messages_use_case
-                            .db
-                            .insert_message(message.message)
-                            .await
-                        {
-                            log::info!("Error inserting message into database: {}", err);
-                            let _ = messages_use_case.status_response(&connection, false).await;
-                            continue;
-                        }
-
-                        let _ = messages_use_case.status_response(&connection, true).await;
-                    }
-                }
-                "subscribe" => {
-                    let message_inner = match incoming.message {
-                        Some(val) => val,
-                        None => {
-                            log::error!("Error parsing subscription message");
-                            let _ = messages_use_case.status_response(&connection, false).await;
-                            continue;
-                        }
-                    };
-
-                    let chat_id = match decode_base64(message_inner.chat_id.clone()).await {
-                        Ok(chat_id) => chat_id,
-                        Err(err) => {
-                            log::error!("Error decoding chat ID: {}", err);
-                            let _ = messages_use_case.status_response(&connection, false).await;
-                            continue;
-                        }
-                    };
-
-                    websocket_use_case
-                        .handle_subscribe(ws.clone(), connection.clone(), &message_inner.chat_id)
-                        .await;
-                    let _ = messages_use_case.status_response(&connection, true).await;
-                    let _ = messages_use_case
-                        .unread_message_response(&connection, &chat_id, message_inner.nonce)
-                        .await;
-                    let _ = messages_use_case
-                        .wait_event_response(&connection, &message_inner.chat_id)
-                        .await;
-                }
-                "unsubscribe" => {
-                    let message_inner = match incoming.message {
-                        Some(val) => val,
-                        None => {
-                            log::error!("Error parsing unsubscribe message");
-                            let _ = messages_use_case.status_response(&connection, false).await;
-                            continue;
-                        }
-                    };
-
-                    websocket_use_case
-                        .handle_unsubscribe(ws.clone(), connection.clone(), &message_inner.chat_id)
-                        .await;
-                    let _ = messages_use_case.status_response(&connection, true).await;
-                }
-                _ => {
-                    log::warn!("unknown request type: {}", incoming.rtype);
-                    let _ = messages_use_case.status_response(&connection, false).await;
-                }
-            }
+impl<MR: MessagesRepository + Clone, DB: MessagesDB + Clone> WebSocketActor<MR, DB> {
+    pub fn new(
+        manager: WebSocketManager,
+        websocket_use_case: WebSocketUseCase<MR>,
+        messages_use_case: MessagesUseCase<DB>,
+    ) -> Self {
+        Self {
+            manager,
+            websocket_use_case,
+            messages_use_case,
         }
     }
+}
 
-    websocket_use_case.disconnect(ws, connection).await;
+impl<MR: MessagesRepository + Clone + Unpin + 'static, DB: MessagesDB + Clone + Unpin + 'static>
+    Actor for WebSocketActor<MR, DB>
+{
+    type Context = Context<Self>;
+}
+
+impl<MR: MessagesRepository + Clone + Unpin + 'static, DB: MessagesDB + Clone + Unpin + 'static>
+    Handler<WebSocketActorMessage> for WebSocketActor<MR, DB>
+{
+    type Result = ResponseFuture<()>;
+
+    fn handle(&mut self, msg: WebSocketActorMessage, _ctx: &mut Self::Context) -> Self::Result {
+        let self_cloned = self.clone();
+        Box::pin(async move {
+            let ws = Arc::new(Mutex::new(self_cloned.manager.clone()));
+            let connection = Arc::new(msg.connection);
+
+            while let Ok(msg) = msg.stream.recv() {
+                if let Message::Text(text) = msg {
+                    let incoming: IncomeMessage = serde_json::from_str(&text).unwrap(); // TODO: proper error handling
+                    match incoming.rtype.as_str() {
+                        "ping" => {
+                            let _ = self_cloned
+                                .messages_use_case
+                                .status_response(&connection, true)
+                                .await;
+                        }
+                        "send" => {
+                            let inner_message = match incoming.message.clone() {
+                                Some(val) => val,
+                                None => {
+                                    log::error!("Error parsing send message");
+                                    continue;
+                                }
+                            };
+
+                            if !self_cloned
+                                .messages_use_case
+                                .is_valid_message(incoming.clone().into())
+                                .await
+                            {
+                                let _ = self_cloned
+                                    .messages_use_case
+                                    .status_response(&connection, false)
+                                    .await;
+                                continue;
+                            }
+
+                            let message = entity::websocket::ConnectedMessage {
+                                connection: connection.clone(),
+                                message: incoming,
+                            };
+
+                            let mut ws_lock = ws.lock().await;
+
+                            if ws_lock.message_queues.contains_key(&inner_message.chat_id) {
+                                let _ = ws_lock
+                                    .message_queues
+                                    .get_mut(&inner_message.chat_id)
+                                    .unwrap()
+                                    .0
+                                    .send(message);
+                                log::info!("Message has been successfully added to the queue");
+                            } else {
+                                log::info!(
+                                    "There is no subscribers to receive message in the queue"
+                                );
+                                if let Err(err) = self_cloned
+                                    .messages_use_case
+                                    .db
+                                    .insert_message(message.message)
+                                    .await
+                                {
+                                    log::info!("Error inserting message into database: {}", err);
+                                    let _ = self_cloned
+                                        .messages_use_case
+                                        .status_response(&connection, false)
+                                        .await;
+                                    continue;
+                                }
+
+                                let _ = self_cloned
+                                    .messages_use_case
+                                    .status_response(&connection, true)
+                                    .await;
+                            }
+                        }
+                        "subscribe" => {
+                            let message_inner = match incoming.message {
+                                Some(val) => val,
+                                None => {
+                                    log::error!("Error parsing subscription message");
+                                    let _ = self_cloned
+                                        .messages_use_case
+                                        .status_response(&connection, false)
+                                        .await;
+                                    continue;
+                                }
+                            };
+
+                            let chat_id = match decode_base64(message_inner.chat_id.clone()).await {
+                                Ok(chat_id) => chat_id,
+                                Err(err) => {
+                                    log::error!("Error decoding chat ID: {}", err);
+                                    let _ = self_cloned
+                                        .messages_use_case
+                                        .status_response(&connection, false)
+                                        .await;
+                                    continue;
+                                }
+                            };
+
+                            self_cloned
+                                .websocket_use_case
+                                .handle_subscribe(
+                                    ws.clone(),
+                                    connection.clone(),
+                                    &message_inner.chat_id,
+                                )
+                                .await;
+                            let _ = self_cloned
+                                .messages_use_case
+                                .status_response(&connection, true)
+                                .await;
+                            let _ = self_cloned
+                                .messages_use_case
+                                .unread_message_response(&connection, &chat_id, message_inner.nonce)
+                                .await;
+                            let _ = self_cloned
+                                .messages_use_case
+                                .wait_event_response(&connection, &message_inner.chat_id)
+                                .await;
+                        }
+                        "unsubscribe" => {
+                            let message_inner = match incoming.message {
+                                Some(val) => val,
+                                None => {
+                                    log::error!("Error parsing unsubscribe message");
+                                    let _ = self_cloned
+                                        .messages_use_case
+                                        .status_response(&connection, false)
+                                        .await;
+                                    continue;
+                                }
+                            };
+
+                            self_cloned
+                                .websocket_use_case
+                                .handle_unsubscribe(
+                                    ws.clone(),
+                                    connection.clone(),
+                                    &message_inner.chat_id,
+                                )
+                                .await;
+                            let _ = self_cloned
+                                .messages_use_case
+                                .status_response(&connection, true)
+                                .await;
+                        }
+                        _ => {
+                            log::warn!("unknown request type: {}", incoming.rtype);
+                            let _ = self_cloned
+                                .messages_use_case
+                                .status_response(&connection, false)
+                                .await;
+                        }
+                    }
+                }
+            }
+
+            self_cloned
+                .websocket_use_case
+                .disconnect(ws, connection)
+                .await;
+        })
+    }
+}
+
+#[derive(ActixMessage)]
+#[rtype(result = "()")]
+pub struct WebSocketActorMessage {
+    pub connection: WebSocketConnection,
+    pub stream: flume::Receiver<Message>,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 #[macro_use]
 extern crate log;
+extern crate pretty_env_logger;
 
 mod base64;
 mod infrastructure;
@@ -12,34 +13,39 @@ mod seed;
 mod traits;
 mod use_case;
 
-use std::sync::Arc;
-
-use actix_web::{get, web, HttpRequest, HttpResponse};
+use actix::Actor;
+use actix::Addr;
+use actix_web::{HttpRequest, HttpResponse, get, web};
 use anyhow::Result;
-use infrastructure::{database::PostgresDatabase, websocket::handle_websocket_connection};
-use seed::entity::websocket::{WebSocketConnection, WebSocketManager};
-use use_case::{
-    messages::{self, MessagesUseCase},
-    websocket::WebSocketUseCase,
+use futures::StreamExt;
+use infrastructure::{
+    database::PostgresDatabase,
+    websocket::{WebSocketActor, WebSocketActorMessage},
 };
+use seed::entity::websocket::{WebSocketConnection, WebSocketManager};
+use use_case::messages::MessagesUseCase;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
-    let port = std::env::var("PORT").unwrap_or("8080".to_string()).parse::<u16>().unwrap();
+    let port = std::env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse::<u16>()
+        .unwrap();
 
     let pg_pool = PostgresDatabase::new().await?;
 
     let messages_use_case = use_case::messages::MessagesUseCase::new(pg_pool);
     let websocket_use_case =
         use_case::websocket::WebSocketUseCase::new(messages_use_case.clone()).await;
-    let websocket_manager = Arc::new(WebSocketManager::new());
+    let websocket_manager = WebSocketManager::new();
+
+    let websocket_actor =
+        WebSocketActor::new(websocket_manager, websocket_use_case, messages_use_case).start();
 
     let server = actix_web::HttpServer::new(move || {
         actix_web::App::new()
-            .app_data(web::Data::new(messages_use_case.clone()))
-            .app_data(web::Data::new(websocket_use_case.clone()))
-            .app_data(web::Data::new(websocket_manager.clone()))
+            .app_data(websocket_actor.clone())
             .service(accept_websocket_connection)
     })
     .bind(("127.0.0.1", port))?
@@ -54,18 +60,27 @@ async fn main() -> Result<()> {
 async fn accept_websocket_connection(
     req: HttpRequest,
     payload: web::Payload,
-    message_use_case: web::Data<messages::MessagesUseCase<PostgresDatabase>>,
-    websocket_use_case: web::Data<WebSocketUseCase<MessagesUseCase<PostgresDatabase>>>,
-    websocket_manager: web::Data<WebSocketManager>,
+    websocket_actor: web::Data<
+        Addr<WebSocketActor<MessagesUseCase<PostgresDatabase>, PostgresDatabase>>,
+    >,
 ) -> actix_web::Result<HttpResponse> {
     let (response, conn, mut stream) = WebSocketConnection::new(&req, payload)?;
-    handle_websocket_connection(
-        conn,
-        &mut stream,
-        message_use_case.get_ref(),
-        websocket_use_case.get_ref(),
-        websocket_manager.get_ref(),
-    )
-    .await;
+    let (tx, rx) = flume::unbounded();
+    
+    while let Some(Ok(msg)) = stream.next().await {
+        if tx.send(msg).is_err() {
+            break;
+        }
+    };
+    
+    let message = WebSocketActorMessage {
+        connection: conn,
+        stream: rx
+    };
+    websocket_actor
+        .send(message)
+        .await
+        .map_err(|_| actix_web::error::ErrorInternalServerError("Failed to send message"))?;
+
     Ok(response)
 }
